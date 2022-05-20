8729b7fb8bcc190ca0aa938788eed474d591c103
Adam Brown <adampsbrown@gmail.com>
2022-05-20 17:25:24 +0100

    fixing line length and missing punctuation

--- vector/src/main/java/im/vector/app/features/onboarding/OnboardingViewModel.kt
+++ vector/src/main/java/im/vector/app/features/onboarding/OnboardingViewModel.kt
@@ -284,11 +284,14 @@ class OnboardingViewModel @AssistedInject constructor(
                                     overrideNextStage?.invoke() ?: _viewEvents.post(OnboardingViewEvents.DisplayRegistrationStage(it.stage))
                                 }
                                 is RegistrationActionHandler.Result.Success           -> onSessionCreated(it.session, isAccountCreated = true)
-                                RegistrationActionHandler.Result.StartRegistration    -> overrideNextStage?.invoke()
-                                        ?: _viewEvents.post(OnboardingViewEvents.DisplayStartRegistration)
+                                RegistrationActionHandler.Result.StartRegistration    -> {
+                                    overrideNextStage?.invoke() ?: _viewEvents.post(OnboardingViewEvents.DisplayStartRegistration)
+                                }
                                 RegistrationActionHandler.Result.UnsupportedStage     -> _viewEvents.post(OnboardingViewEvents.DisplayRegistrationFallback)
                                 is RegistrationActionHandler.Result.SendEmailSuccess  -> _viewEvents.post(OnboardingViewEvents.OnSendEmailSuccess(it.email))
-                                is RegistrationActionHandler.Result.SendMsisdnSuccess -> _viewEvents.post(OnboardingViewEvents.OnSendMsisdnSuccess(it.msisdn.msisdn))
+                                is RegistrationActionHandler.Result.SendMsisdnSuccess -> {
+                                    _viewEvents.post(OnboardingViewEvents.OnSendMsisdnSuccess(it.msisdn.msisdn))
+                                }
                                 is RegistrationActionHandler.Result.Error             -> _viewEvents.post(OnboardingViewEvents.Failure(it.cause))
                                 RegistrationActionHandler.Result.MissingNextStage     -> {
                                     _viewEvents.post(OnboardingViewEvents.Failure(IllegalStateException("No next registration stage found")))
